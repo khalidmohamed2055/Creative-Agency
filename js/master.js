8167f8415883910003fb6cff5f7f7410f4bce40f
khalidmohamed2055 <khaledmohamed22f1@gmail.com>
2026-06-29 03:26:50 +0300

    Add initial HTML structure, CSS links, and JavaScript functionality for landing page and gallery
    - Created index.html with sections for landing page, about us, skills, gallery, timeline, and footer.
    - Added SVG image for web design.
    - Implemented JavaScript for random background images on the landing page and skill progress animations.
    - Developed a gallery feature with popup functionality for images.

---
+++ js/master.js
@@ -0,0 +1,147 @@
+// Select Landing Page Element
+let landingPage = document.querySelector(".landing-page");
+
+// Get Array Of Imgs
+let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+
+// Function To Randomize Imgs
+      setInterval(() => {
+
+      // Get Random Number
+      let randomNumber = Math.floor(Math.random() * imgsArray.length);
+    
+      // Change Background Image Url 
+      landingPage.style.backgroundImage = 'url("imgs/' + imgsArray[randomNumber] + '")';
+    
+    }, 5000);
+    
+
+    // Select Skills Selector
+let ourSkills = document.querySelector(".skills");
+
+window.onscroll = function () {
+
+  // Skills Offset Top
+  let skillsOffsetTop = ourSkills.offsetTop;
+
+  // Skills Outer Height
+  let skillsOuterHeight = ourSkills.offsetHeight;
+
+  // Window Height
+  let windowHeight = this.innerHeight;
+
+  // Window ScrollTop
+  let windowScrollTop = this.pageYOffset;
+
+  if (windowScrollTop > (skillsOffsetTop + skillsOuterHeight - windowHeight)) {
+
+    let allSkills = document.querySelectorAll(".skill-box .skill-progress span");
+
+    allSkills.forEach(skill => {
+
+      skill.style.width = skill.dataset.progress;
+
+    });
+
+  }
+
+};
+
+
+
+// Create Popup With The Image
+let ourGallery = document.querySelectorAll(".gallery img");
+
+ourGallery.forEach(img => {
+
+  img.addEventListener('click', (e) => {
+
+    // Create Overlay Element
+    let overlay = document.createElement("div");
+
+    // Add Class To Overlay
+    overlay.className = 'popup-overlay';
+
+    // Append Overlay To The Body
+    document.body.appendChild(overlay);
+
+    // Create The Popup Box
+    let popupBox = document.createElement("div");
+
+    // Add Class To The Popup Box
+    popupBox.className = 'popup-box';
+
+    if (img.alt !== null) {
+
+      // Create Heading
+      let imgHeading = document.createElement("h3");
+
+      // Create text For Heading
+      let imgText = document.createTextNode(img.alt);
+
+      // Append The Text To The Heading
+      imgHeading.appendChild(imgText);
+
+      // Append The Heading To The Popup Box
+      popupBox.appendChild(imgHeading);
+
+    }
+
+    // Create The Image
+    let popupImage = document.createElement("img");
+
+    // Set Image Source
+    popupImage.src = img.src;
+
+    // Add Image To Popup Box
+    popupBox.appendChild(popupImage);
+
+    // Append The Popup Box To Body
+    document.body.appendChild(popupBox);
+
+    // Create The Close Span
+    let closeButton = document.createElement("span");
+
+    // Create The Close Button Text
+    let closeButtonText = document.createTextNode("X");
+
+    // Append Text To Close Button
+    closeButton.appendChild(closeButtonText);
+
+    // Add Class To Close Button
+    closeButton.className = 'close-button';
+
+    // Add Close Button To The Popup Box
+    popupBox.appendChild(closeButton);
+
+  });
+
+});
+
+// Close Popup
+document.addEventListener("click", function (e) {
+
+  if (e.target.className == 'close-button') {
+
+    // Remove The Current Popup
+    e.target.parentNode.remove();
+
+    // Remove Overlay
+    document.querySelector(".popup-overlay").remove();
+
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
